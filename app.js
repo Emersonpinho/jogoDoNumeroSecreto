407f553affb68a815becd4af9b2064ac463e3901
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-05 09:59:35 -0300

    criando a base do jogo

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert ("Olá mundo");
+// Primeira interação com o usuario
+alert ('Boas vindas ao jogo do Numero secreto');
+
+// Defini a variavel do nnumero secreto
+let numeroSecreto = 29;
+
+//Interação que pede o numero ao usuario para ele tantar acertar
+prompt('Escolha um numero entre 1 e 30');
