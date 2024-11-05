666006189701087908eea12d7bfeaabc84905464
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-05 11:31:39 -0300

    criando a estrutura enquanto e adicionando as condições demtrp desse loop

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 // Primeira interação com o usuario
 alert ('Boas vindas ao jogo do Numero secreto');
 
-// Defini a variavel do nnumero secreto
+// Defini a variavel do numero secreto e o chute do usuario
 let numeroSecreto = 95;
+let chute;
 
 // Hackerzinho que diz no console qual o numero secreto
 console.log(numeroSecreto);
 
-// Interação que pede o numero ao usuario para ele tantar acertar
-let chute = prompt('Escolha um numero entre 1 e 100');
-
-// Condição que verifica se o usuario acertou ou não, mas não escreve na tela e sim no console.
-if (chute == numeroSecreto) {
-    alert(`Isso ai! você descobriu o numero secreto ${numeroSecreto}`);
-} else {
-    if(chute > numeroSecreto){
-        alert(`O numero secreto é MENOR que ${chute}`);
+// Enquanto(repetição) chute não for igual a o numero secreto 
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um numero entre 1 e 100');
+    // Condição que verifica se o usuario acertou ou não, mas não escreve na tela e sim no console.
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! você descobriu o numero secreto ${numeroSecreto}`);
     } else {
-        alert(`O numero secreto é MAIOR que ${chute}`)
+        if(chute > numeroSecreto){
+            alert(`O numero secreto é MENOR que ${chute}`);
+        } else {
+            alert(`O numero secreto é MAIOR que ${chute}`);
     }
-}
+}}
