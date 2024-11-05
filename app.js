c46fcf0beff4861907fa8290e5537fb7978b9a5b
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-05 10:23:50 -0300

    adicionando as condicionais

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ alert ('Boas vindas ao jogo do Numero secreto');
 // Defini a variavel do nnumero secreto
 let numeroSecreto = 29;
 
-//Interação que pede o numero ao usuario para ele tantar acertar
-prompt('Escolha um numero entre 1 e 30');
+// Interação que pede o numero ao usuario para ele tantar acertar
+let chute = prompt('Escolha um numero entre 1 e 30');
+
+// Condição que verifica se o usuario acertou ou não, mas não escreve nasa na tela e sim no console.
+if (chute == numeroSecreto) {
+    console.log('Isso ai! você descobriu o numero secreto (5)');
+}
