e03b3a6fca2500b5cba1f3ddc0162772fc15592f
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-05 11:37:41 -0300

    adicionei numeros de tentativas

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Primeira interação com o usuario
 alert ('Boas vindas ao jogo do Numero secreto');
 
-// Defini a variavel do numero secreto e o chute do usuario
+// Definindo as variavels
 let numeroSecreto = 95;
 let chute;
+let tentativas = 1;
 
 // Hackerzinho que diz no console qual o numero secreto
 console.log(numeroSecreto);
@@ -13,11 +14,13 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um numero entre 1 e 100');
     // Condição que verifica se o usuario acertou ou não, mas não escreve na tela e sim no console.
     if (chute == numeroSecreto) {
-        alert(`Isso ai! você descobriu o numero secreto ${numeroSecreto}`);
+        alert(`Isso ai! você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tantativas`);
     } else {
         if(chute > numeroSecreto){
             alert(`O numero secreto é MENOR que ${chute}`);
         } else {
             alert(`O numero secreto é MAIOR que ${chute}`);
     }
+    // tentativas = tantativas + 1;
+    tentativas ++;
 }}
