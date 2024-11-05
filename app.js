63d7c94991133ad7e4eb73e56f165c7bc6e62d4b
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-05 10:35:53 -0300

    adicionando um "hackerzinho" e terminando as condicionais

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 alert ('Boas vindas ao jogo do Numero secreto');
 
 // Defini a variavel do nnumero secreto
-let numeroSecreto = 29;
+let numeroSecreto = 4;
+
+// Hackerzinho que diz no console qual o numero secreto
+console.log(numeroSecreto);
 
 // Interação que pede o numero ao usuario para ele tantar acertar
 let chute = prompt('Escolha um numero entre 1 e 30');
 
-// Condição que verifica se o usuario acertou ou não, mas não escreve nasa na tela e sim no console.
+// Condição que verifica se o usuario acertou ou não, mas não escreve na tela e sim no console.
 if (chute == numeroSecreto) {
-    console.log('Isso ai! você descobriu o numero secreto (5)');
+    alert('Isso ai! você descobriu o numero secreto (5)');
+} else {
+    alert('Você errou :(')
 }
