014bd4efbd56b2415803ae56e6c3b8f94d8961d8
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-05 11:00:13 -0300

    adicionando dicas para o usuario

--- app.js
+++ app.js
@@ -2,17 +2,21 @@
 alert ('Boas vindas ao jogo do Numero secreto');
 
 // Defini a variavel do nnumero secreto
-let numeroSecreto = 4;
+let numeroSecreto = 95;
 
 // Hackerzinho que diz no console qual o numero secreto
 console.log(numeroSecreto);
 
 // Interação que pede o numero ao usuario para ele tantar acertar
-let chute = prompt('Escolha um numero entre 1 e 30');
+let chute = prompt('Escolha um numero entre 1 e 100');
 
 // Condição que verifica se o usuario acertou ou não, mas não escreve na tela e sim no console.
 if (chute == numeroSecreto) {
     alert(`Isso ai! você descobriu o numero secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(')
+    if(chute > numeroSecreto){
+        alert(`O numero secreto é MENOR que ${chute}`);
+    } else {
+        alert(`O numero secreto é MAIOR que ${chute}`)
+    }
 }
